59864526ca9661fbf09aaa7c02e306ac59891405
Raj Yadav <raj.yaduvanshi205@gmail.com>
2026-07-29 18:12:02 +0530

    chore(frontend): disable eslint plugin and add typescript-eslint dependencies

--- render-build.sh
+++ render-build.sh
@@ -7,7 +7,7 @@ echo "Building Bidinn CRM for Render..."
 echo "1/2: Building Frontend..."
 cd frontend
 yarn install
-FAST_REFRESH=false yarn build
+DISABLE_ESLINT_PLUGIN=true FAST_REFRESH=false yarn build
 cd ..
 
 echo "2/2: Building Backend..."
