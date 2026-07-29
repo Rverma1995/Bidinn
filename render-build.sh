ff7f6a2f98991aab4febbc54fa7cbef254eb43a8
Raj Yadav <raj.yaduvanshi205@gmail.com>
2026-07-29 18:28:10 +0530

    build(frontend/backend): ensure full dependencies during build process

--- render-build.sh
+++ render-build.sh
@@ -6,13 +6,13 @@ echo "Building Bidinn CRM for Render..."
 
 echo "1/2: Building Frontend..."
 cd frontend
-yarn install
+yarn install --production=false
 DISABLE_ESLINT_PLUGIN=true FAST_REFRESH=false yarn build
 cd ..
 
 echo "2/2: Building Backend..."
 cd backend
-yarn install
+yarn install --production=false
 yarn build
 cd ..
 
